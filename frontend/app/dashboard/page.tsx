1442b29ccb8de5a1fe37ece07b8e429d9c8dcb4e
Ariel Corte <cortearielformal@gmail.com>
2025-11-22 22:05:10 -0300

    add shadcn sidebar

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { UserPill } from "@privy-io/react-auth/ui";
-
-export default function Dashboard() {
+export default function DashboardHome() {
   return (
     <div>
-      <div className="flex justify-end p-3">
-        <UserPill />
-      </div>
-
-      <h1>Your investments</h1>
+      <h1>Home</h1>
     </div>
   );
 }
---
+++ frontend/components/app-sidebar.tsx
@@ -0,0 +1,23 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+import { UserPill } from "@privy-io/react-auth/ui";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter>
+        <UserPill />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
